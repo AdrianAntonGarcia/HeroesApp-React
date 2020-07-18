8edec617f48ac746f55121fe104c4a59a3f5f270
adriton <adrtler@gmail.com>
2020-07-18 18:18:44 +0200

    Leer argumentos por URL

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { getHeroesById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
-    return (
-        <div>
-            <h1>HeroScreen</h1>
-        </div>
-    )
-}
+  /**
+   * Hook para extraer los parametros que vengan por el url
+   */
+  const params = useParams();
+  const heroeId = params.heroeId;
+
+  // Hay que controlar que no sea undefined
+  const hero = getHeroesById(heroeId);
+
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
+  return (
+    <div>
+      <h1>HeroScreen</h1>
+    </div>
+  );
+};
