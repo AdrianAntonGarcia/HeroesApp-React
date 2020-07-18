e91634b474fffe959405c8ecc2c3b9201478d440
adriton <adrtler@gmail.com>
2020-07-18 17:28:26 +0200

    History push / replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-export const LoginScreen = () => {
-    return (
-        <div>
-            <h1>LoginScreen</h1>
-        </div>
-    )
-}
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    // history.push('/');
+    /**
+     * Reemplaza el último lugar visitado, si el usuario vuelve hacia atrás
+     * no irá al login
+     */
+    history.replace('/');
+  };
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
