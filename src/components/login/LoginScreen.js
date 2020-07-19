5e55b8057eb75971262c38d05598d2360a05e333
adriton <adrtler@gmail.com>
2020-07-19 10:13:05 +0200

    Login de un usuario

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
+  const { dispatch } = useContext(AuthContext);
+
   const handleLogin = () => {
     // history.push('/');
     /**
      * Reemplaza el último lugar visitado, si el usuario vuelve hacia atrás
      * no irá al login
      */
+    // history.replace('/');
+    const action = {
+      type: types.login,
+      payload: {
+        name: 'Adrian',
+      },
+    };
+    dispatch(action);
     history.replace('/');
   };
   return (
